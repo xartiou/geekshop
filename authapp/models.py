e4a647d951de8907cbed1251500b8aa5593705af
xartiou <xartiou@gmail.com>
2021-12-29 20:11:52 +0300

    - модель с данными пользователя - сигналы на создание и сохранение данных пользователя

--- authapp/models.py
+++ authapp/models.py
@@ -1,5 +1,7 @@
 from datetime import timedelta
 
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
@@ -10,7 +12,7 @@ from django.utils.timezone import now
 
 class User(AbstractUser):
     image = models.ImageField(upload_to='users_image', blank=True)
-    age = models.PositiveIntegerField(default=18)
+    age = models.PositiveIntegerField(verbose_name='возраст', default=18)
 
     activation_key = models.CharField(max_length=128, blank=True)  # ключ
     activation_key_expires = models.DateTimeField(auto_now=True, blank=True, null=True)  # срок годности ключа
@@ -20,3 +22,26 @@ class User(AbstractUser):
         if now() <= self.activation_key_expires + timedelta(hours=48):
             return False
         return True
+
+
+# модель с данными пользователя
+class UserProfile(models.Model):
+    MALE = 'M'
+    FEMALE = 'W'
+    GENDER_CHOICES = (
+        (MALE, 'М'),
+        (FEMALE, 'Ж'),
+    )
+    user = models.OneToOneField(User, unique=True, null=False, db_index=True, on_delete=models.CASCADE)
+    about = models.TextField(verbose_name='о себе', blank=True, null=True)
+    gender = models.CharField(verbose_name='пол', choices=GENDER_CHOICES, blank=True,max_length=2)
+
+# сигналы на создание и сохранение данных пользователя
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            UserProfile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, created, **kwargs):
+        instance.userprofile.save()
