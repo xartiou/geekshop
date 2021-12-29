b07a763a7d8461f20fc285364ab8636915de503e
xartiou <xartiou@gmail.com>
2021-12-30 00:19:34 +0300

    - Реализовал автоматическое заполнение профиля пользователя при аутентификации через социальную сеть.

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -150,6 +150,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'authapp.User'
 LOGIN_URL = '/authapp/login/'
 LOGIN_REDIRECT_URL = '/'
+LOGIN_ERROR_URL = '/'
 
 # DOMAIN_NAME = 'http://localhost:8000'
 # EMAIL_HOST = 'localhost'
@@ -174,3 +175,14 @@ SOCIAL_AUTH_VK_OAUTH2_SECRET = os.getenv('SOCIAL_AUTH_VK_OAUTH2_SECRET')
 SOCIAL_AUTH_VK_OAUTH2_API_VERSION = '5.131'  # версия API для VK
 SOCIAL_AUTH_VK_OAUTH2_IGNORE_DEFAULT_SCOPE = True  # запрос на разрешение на работу с данными
 SOCIAL_AUTH_VK_OAUTH2_SCOPE = ['email']
+
+SOCIAL_AUTH_PIPELINE = (
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.create_user',
+    'authapp.pipeline.save_user_profile',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+)
