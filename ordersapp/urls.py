3f34441979f762a16c914c8593fa279a626e25d7
xartiou <xartiou@gmail.com>
2022-01-04 17:50:12 +0300

    - создал контроллеры CRUD для заказа на базе Django CBV. - создал выпадающее меню

--- ordersapp/urls.py
+++ ordersapp/urls.py
@@ -1,10 +1,15 @@
 
 from django.urls import path
-
-
+from ordersapp.views import OrderListView, OrderCreateView, OrderDetailView, OrderUpdateView, OrderDeleteView
+from ordersapp.views import order_forming_complete
 app_name = 'ordersapp'
 urlpatterns = [
 
-    #path('login/', LoginListView.as_view(),name='login'),
+    path('', OrderListView.as_view(), name='list'),
+    path('create/', OrderCreateView.as_view(), name='create'),
+    path('read/<int:pk>/', OrderDetailView.as_view(), name='read'),
+    path('update/<int:k>/', OrderUpdateView.as_view(), name='update'),
+    path('delete/<int:pk>/', OrderDeleteView.as_view(), name='delete'),
+    path('forming_complete/<int:pk>/', order_forming_complete, name='forming_complete'),
 
 ]
--- ordersapp/views.py
+++ ordersapp/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, UpdateView, CreateView, DeleteView, TemplateView
+from mainapp.mixin import BaseClassContextMixin
+from ordersapp.models import Order
+
 
 # Create your views here.
+
+
+# список заказов(Order)
+class OrderListView(ListView, BaseClassContextMixin):  # добавили Mixin чтобы не переопределять title
+    model = Order
+    title = 'Geekshop | Список заказов'
+
+
+class OrderCreateView(CreateView):
+    pass
+
+
+class OrderDetailView(DetailView):
+    pass
+
+
+class OrderUpdateView(UpdateView):
+    pass
+
+
+class OrderDeleteView(DeleteView):
+    pass
+
+
+#  функция для смены статусов заказа
+def order_forming_complete(request, pk):
+    pass
