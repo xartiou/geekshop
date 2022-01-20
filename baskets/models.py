13a937c3a9fdfc4f52c915d72c122bfd993f38f1
xartiou <xartiou@gmail.com>
2022-01-20 16:02:26 +0300

    Организовал работу с остатками товара (при добавлении в корзину и при удалении из корзины), переопределив методы «.save()» и «.delete()» в моделях «Basket» и «OrderItem»

--- baskets/models.py
+++ baskets/models.py
@@ -5,9 +5,20 @@ from django.db import models
 from authapp.models import User
 from mainapp.models import Product
 
+# менеджер для метода удаления при работе с QuerySet
+class BasketQuerySet(models.QuerySet):
+    def delete(self, *args, **kwargs):
+        for item in self:
+            item.product.quantity += item.quantity
+            item.product.save()
+        super(BasketQuerySet, self).delete(*args, **kwargs)
+
 
 
 class Basket(models.Model):
+    # прописываем менеджер QuerySet в класс
+    objects = BasketQuerySet.as_manager()
+
     user = models.ForeignKey(User,on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=0)
@@ -32,3 +43,25 @@ class Basket(models.Model):
     def total_quantity(self):
         baskets = Basket.objects.filter(user=self.user)
         return sum(basket.quantity for basket in baskets)
+
+    # метод удаления при занесении товара в корзину
+    def delete(self, *args, **kwargs):
+        self.product.quantity += self.quantity
+        self.product.save()
+        super(Basket, self).delete(*args, **kwargs)
+
+    # метод сохранения при удалении со склада
+    def save(self, *args, **kwargs):
+        if self.pk:
+            get_item = self.get_item(int(self.pk))
+            self.product.quantity -= self.quantity - get_item
+        else:
+            self.product.quantity -= self.quantity
+
+        self.product.save()
+        super(Basket, self).save(*args, **kwargs)
+
+    @staticmethod
+    def get_item(pk):
+        return Basket.objects.get(pk=pk).quantity
+
