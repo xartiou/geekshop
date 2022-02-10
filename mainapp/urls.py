69d5d60d57cc5d7eb5b2821ee47336266c60e2bd
xartiou <xartiou@gmail.com>
2022-02-10 17:58:35 +0300

    'кешируем данные страницы через url'

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -6,8 +6,8 @@ from django.views.decorators.cache import cache_page
 app_name = 'mainapp'
 urlpatterns = [
 
-    path('', products,name='products'),
-    path('category/<int:id_category>', products, name='category'),
+    path('', cache_page(3600)(products),name='products'),
+    path('category/<int:id_category>', cache_page(3600)(products), name='category'),
     path('page/<int:page>', cache_page(3600)(products), name='page'),
     path('detail/<int:pk>/', ProductDetail.as_view(), name='detail'),
 ]
