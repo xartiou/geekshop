ea466dfdb392acf06abf92cf2110cf9571a81d51
xartiou <xartiou@gmail.com>
2022-02-16 12:11:08 +0300

    работа с F-объектом, убедился, что обновление значений выполняется на уровне БД, а не в python-коде

--- baskets/views.py
+++ baskets/views.py
@@ -1,24 +1,11 @@
 from django.contrib.auth.decorators import login_required
+from django.db import connection
 from django.http import HttpResponseRedirect, JsonResponse
 # Create your views here.
 from django.template.loader import render_to_string
-
+from django.db.models import F, Q
 from baskets.models import Basket
 from mainapp.models import Product
-
-
-# @login_required
-# def basket_add(request, id):
-#     user_select = request.user
-#     product = Product.objects.get(id=id)
-#     baskets = Basket.objects.filter(user=user_select, product=product)
-#     if baskets:
-#         basket = baskets.first()
-#         basket.quantity += 1
-#         basket.save()
-#     else:
-#         Basket.objects.create(user=user_select, product=product, quantity=1)
-#     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
 
 @login_required
@@ -28,11 +15,15 @@ def basket_add(request,id):
     baskets = Basket.objects.filter(user=user_select,product=product)
     if baskets:
         basket = baskets.first()
-        basket.quantity += 1
+        # basket.quantity += 1
+        basket.quantity = F('quantity') + 1  # выполнить действие прямо на уровне БД
         basket.save()
+        update_queries = list(filter(lambda x: 'UPDATE' in x['sql'], connection.queries))
+        print(f'basket_add {update_queries} ')
     else:
         Basket.objects.create(user=user_select,product=product,quantity=1)
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
 
 @login_required
 def basket_remove(request, basket_id):
