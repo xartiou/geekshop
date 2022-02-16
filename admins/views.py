2b35cb9fe81246651085fc328b1582d5ab6a218b
xartiou <xartiou@gmail.com>
2022-02-16 13:02:49 +0300

    При управлении активностью категории активность товаров тоже должна обновляться.

--- admins/views.py
+++ admins/views.py
@@ -76,8 +76,8 @@ class CategoryDeleteView(DeleteView, BaseClassContextMixin, CustomDispatchMixin)
 
     def delete(self, request, *args, **kwargs):
         self.object = self.get_object()
-        self.object.is_active = False
-        self.object.product_set.update(is_active=False)
+        self.object.is_active = False  # при удалении категории
+        self.object.product_set.update(is_active=False)  # будут удаляться продукты удаленной категории
         self.object.save()
         return HttpResponseRedirect(self.get_success_url())
 
